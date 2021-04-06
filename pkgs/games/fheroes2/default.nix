2f845dea4e2842930b81d14f72324f63e7d7869f
Karol Chmist <info+github@chmist.com>
2021-04-06 13:47:43 +0200

    WIP fheroes 0.9.2

--- pkgs/games/fheroes2/default.nix
+++ pkgs/games/fheroes2/default.nix
@@ -1,9 +1,9 @@
 { stdenv, fetchFromGitHub, fetchurl,
-SDL2, SDL2_image, SDL2_mixer, SDL2_ttf, 
+SDL2, SDL2_image, SDL2_mixer, SDL2_ttf,
 gettext, libpng, unzip, zlib }:
 
 stdenv.mkDerivation rec {
-  version = "0.7";
+  version = "0.9.2";
   pname = "fheroes2";
 
   # src = "/home/karol/workspace/open-source/fheroes2";
@@ -12,36 +12,42 @@ stdenv.mkDerivation rec {
     owner = "ihhub";
     repo = "fheroes2";
     rev = "${version}";
-    sha256 = "1rbm9xxs42qvi0nab1smz5zbw9xqkmxikcgbk2j7b5hkg46ypclp";
+    sha256 = "1n80ai1yymnwmy4ssbvhf6q7knrcg2mwxls0s7jxl66nnzn2bgr3";
   };
-  buildInputs = [ gettext SDL2 SDL2_image SDL2_mixer SDL2_ttf 
+  buildInputs = [ gettext SDL2 SDL2_image SDL2_mixer SDL2_ttf
   libpng zlib ];
 
-  preBuild = ''
-    export WITH_SDL2="ON"    
-    mkdir -p $out
-    ls -R $out
-    
-    mkdir -p ~/.local/share/fheroes/{data,maps}
-    
-    ln -s  ~/.local/share/fheroes/data $out/data
-    ln -s  ~/.local/share/fheroes/maps $out/maps
-  '';
-  
+
+  # Fix tests by preventing them from writing to /homeless-shelter.
+  #preCheck = ''
+  #  export HOME=$(mktemp -d)
+  #'';
+
+  #preBuild = ''
+  #  export WITH_SDL2="ON"
+  #  mkdir -p $out
+  #  ls -R $out#
+#
+ #   mkdir -p ~/.local/share/fheroes/{data,maps}
+#
+ #   ln -s  ~/.local/share/fheroes/data $out/data
+  #  ln -s  ~/.local/share/fheroes/maps $out/maps
+  #'';
+
   installPhase = ''
     runHook preInstall
 
     echo src:$src
-    
+
     ls -R /build/source/src/dist
 
     mkdir -p $out/bin
 
     install -Dm755  /build/source/src/dist/fheroes2 $out/fheroes2
-    install -D  $src/fheroes2.cfg $out/fheroes2.cfg
-    install -D  $src/fheroes2.key $out/fheroes2.key
-    
-    ls -R $out        
+    # install -D  $src/fheroes2.cfg $out/fheroes2.cfg
+    # install -D  $src/fheroes2.key $out/fheroes2.key
+
+    ls -R $out
 
     runHook postInstall
   '';
