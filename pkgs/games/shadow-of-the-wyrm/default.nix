6520dbd0e05a751a2c0bffb5ebdbaa09d2799007
Karol Chmist <info+github@chmist.com>
2023-04-04 09:16:23 +0200

    WIP

--- pkgs/games/shadow-of-the-wyrm/default.nix
+++ pkgs/games/shadow-of-the-wyrm/default.nix
@@ -1,18 +1,13 @@
-{ lib,fetchFromGitHub, stdenv, premake4, boost, xercesc, zlib, ncurses, lua5_1, SDL2, SDL2_image }:
+{ lib,fetchFromGitHub, stdenv, premake4, boost, xercesc, zlib, ncurses, lua5_1, SDL2, SDL2_image, gtest }:
 
 stdenv.mkDerivation {
   name = "shadow-of-the-wyrm";
   version = "1.4.7";
-  src = fetchFromGitHub {
-    owner = "prolog";
-    repo = "shadow-of-the-wyrm";
-    rev = "d91f44921d21d0a176415a9ed3b70ef55dd77138";
-    sha256 = "sha256-v00tDCZ2MEszdQYFjg99HxP3in/jLqUuERasSJZ1S38=";
-  };
+  src = /home/karol/workspace/open-source/shadow-of-the-wyrm;
   buildInputs = [
     premake4
     boost
-#    googletest
+#    gtest
     xercesc
     zlib
     ncurses
@@ -29,13 +24,31 @@ stdenv.mkDerivation {
   buildPhase = ''
     premake4 gmake
 
-    make config=release -j $NIX_BUILD_CORES
+    make config=debug -j $NIX_BUILD_CORES
   '';
+
+#  separateDebugInfo = true;
+  dontStrip = true;
 
   installPhase = ''
     runHook preInstall
-    mkdir -p $out/sotw
-    cp -R sotw/* $out/sotw
+
+    pwd
+    ls -al
+    mkdir -p $out
+    cp -R * $out
+
     runHook postInstall
   '';
+
+  fixupPhase = ''
+      runHook preFixup
+
+      sed -i "s@=data/@=$out/data/@" $out/swyrm.ini
+      sed -i "s@log_dir=@log_dir=/var/sotw@" $out/swyrm.ini
+      sed -i "s@syschardump_dir==@syschardump_dir==/var/sotw@" $out/swyrm.ini
+
+      runHook postFixup
+    '';
+
 }
